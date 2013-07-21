587e703e1e483326da50b671c92745dbb9b15afb
Michał Szajbe <michal.szajbe@gmail.com>
2013-07-22 00:32:53 +0200

    Added more key-bindings

--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,6 +1,19 @@
 # vi-mode
 bindkey -v
 
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
 # bind UP and DOWN arrow keys
 for keycode in '[' '0'; do
   bindkey "^[${keycode}A" history-substring-search-up
