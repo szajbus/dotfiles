7534a59c727db7ce446b403e0a3c76c68db4e9e5
Michał Szajbe <michal.szajbe@gmail.com>
2021-08-24 18:01:38 +0200

    Improve bash propmpt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,22 +1,22 @@
-COLOR_RED="\033[0;31m"
-COLOR_YELLOW="\033[0;33m"
-COLOR_GREEN="\033[0;32m"
-COLOR_CYAN="\033[0;36m"
-COLOR_PURPLE="\033[0;35m"
-COLOR_RESET="\033[0m"
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_GREEN='\033[0;32m'
+COLOR_CYAN='\033[0;36m'
+COLOR_PURPLE='\033[0;35m'
+COLOR_RESET='\033[0m'
 
 function prompt-setup {
   local pwd="${COLOR_GREEN}\w${COLOR_RESET}"
-  local git_info="$(prompt-git-info)"
-  local user_indicator="\$"
+  local git_info='$(prompt-git-info)'
+  local user_indicator='\$'
 
   if [[ $(uname) == "Darwin" ]]; then
     local prefix=""
   else
-    local prefix="${COLOR_PURPLE}\u${COLOR_RESET}@${COLOR_YELLOW}\h${COLOR_RESET}"
+    local prefix="${COLOR_PURPLE}\u${COLOR_RESET}@${COLOR_YELLOW}\h${COLOR_RESET}:"
   fi
 
-  export PS1="${prefix} ${pwd} ${git_info}${user_indicator} "
+  export PS1="${prefix}${pwd} ${git_info}${user_indicator} "
 }
 
 function prompt-git-info {
@@ -27,15 +27,13 @@ function prompt-git-info {
 
     # Get current status
     while IFS=$"\n" read line; do
-      [[ "$line" =~ ([ACDMT][\ MT]|[ACMT]D)\ * ]] && local added="${COLOR_GREEN}●${COLOR_RESET}"
-      [[ "$line" =~ [\ ACMRT]D\ * ]]              && local deleted="${COLOR_RED}●${COLOR_RESET}"
-      [[ "$line" =~ ?[MT]\ * ]]                   && local modified="${COLOR_RED}●${COLOR_RESET}"
-      [[ "$line" =~ R\ * ]]                      && local added="${COLOR_GREEN}●${COLOR_RESET}"
-      [[ "$line" =~ (AA|DD|U?|?U)\ * ]]           && local modified="${COLOR_RED}●${COLOR_RESET}"
-      [[ "$line" =~ \?\?\ * ]]                    && local untracked="${COLOR_YELLOW}●${COLOR_RESET}"
+      [[ "$line" =~ ^[MADRC]\ \ .* ]]       && local staged="${COLOR_GREEN}●${COLOR_RESET}"
+      [[ "$line" =~ ^.[MADRC]\ .* ]]        && local unstaged="${COLOR_RED}●${COLOR_RESET}"
+      [[ "$line" =~ ^(AA|DD|U.|.U)\ .* ]]   && local unstaged="${COLOR_RED}●${COLOR_RESET}"
+      [[ "$line" =~ ^\?\?\ .* ]]            && local untracked="${COLOR_YELLOW}●${COLOR_RESET}"
     done < <(git status --porcelain 2> /dev/null)
 
-    echo "[${branch}${deleted}${added}${modified}${untracked}]"
+    echo -e "[${branch}${staged}${unstaged}${untracked}]"
   fi
 }
 
