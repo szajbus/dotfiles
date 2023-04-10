0c9dbdbec7802b6021500fb9a1f1f0dc407ab7b0
Michał Szajbe <michal.szajbe@gmail.com>
2023-04-10 23:57:23 +0200

    Attach tmux session in terminal only

--- shell/rc.sh
+++ shell/rc.sh
@@ -1,7 +1,7 @@
 source $HOME/dotfiles/shell/aliases.sh
 
 ### tmux
-if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
+if [ -t 1 ] && [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
   tmux attach-session -d -t $USER || tmux new-session -s $USER
 fi
 
