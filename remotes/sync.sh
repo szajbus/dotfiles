2790781e26a08795c97ae939b9027998ae4bbebe
Michał Szajbe <michal.szajbe@gmail.com>
2023-07-12 14:32:41 +0200

    Support syncing multiple hosts

--- remotes/sync.sh
+++ remotes/sync.sh
@@ -2,12 +2,11 @@
 # Install dotfiles on remote host
 #
 # Usage:
-#   sync.sh <host> <profile>
+#   sync.sh <profile> <host> [<host> ...]
 
 set -e
 
-REMOTE=$1
-PROFILE=$2
+PROFILE=$1; shift
 PROFILE_PATH=~/dotfiles/remotes/${PROFILE}.profile
 
 if [ ! -f $PROFILE_PATH ]; then
@@ -17,20 +16,22 @@ fi
 
 source $PROFILE_PATH
 
-tar -c --directory ~/dotfiles ${DOTFILES_PATTERN[@]} \
-  | ssh \
-    -o PermitLocalCommand=no \
-    -o RemoteCommand=none \
-    -o RequestTTY=no \
-    $REMOTE "
-      cat > dotfiles.${PROFILE}.tar;
-      mkdir ~/dotfiles 2> /dev/null;
-      tar -mx --directory ~/dotfiles -f dotfiles.${PROFILE}.tar;
-      for src in \$(find ~/dotfiles -maxdepth 1 -not -type d); do
-        dest=.\${src##*/}
-        [ -f \${dest} ] && [ ! -L \${dest} ] && cp \${dest} \${dest}.bak
-        ln -nfs \${src} \${dest}
-      done;
-      rm dotfiles.${PROFILE}.tar"
+for REMOTE in $@; do
+  tar -c --directory ~/dotfiles ${DOTFILES_PATTERN[@]} \
+    | ssh \
+      -o PermitLocalCommand=no \
+      -o RemoteCommand=none \
+      -o RequestTTY=no \
+      $REMOTE "
+        cat > dotfiles.${PROFILE}.tar;
+        mkdir ~/dotfiles 2> /dev/null;
+        tar -mx --directory ~/dotfiles -f dotfiles.${PROFILE}.tar;
+        for src in \$(find ~/dotfiles -maxdepth 1 -not -type d); do
+          dest=.\${src##*/}
+          [ -f \${dest} ] && [ ! -L \${dest} ] && cp \${dest} \${dest}.bak
+          ln -nfs \${src} \${dest}
+        done;
+        rm dotfiles.${PROFILE}.tar"
 
-echo "synced ${PROFILE} dotfiles on ${REMOTE}" >&2
+  echo "synced ${PROFILE} dotfiles on ${REMOTE}" >&2
+done
