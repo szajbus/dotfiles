32c66a7e6d398dd9c64c58071f5a7f4c80383dd4
Michał Szajbe <michal.szajbe@gmail.com>
2025-02-24 21:24:18 +0100

    Clean up env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -1,23 +1,12 @@
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
-
-if [[ $(uname) == "Darwin" ]]; then
-  if [ -d /opt/X11/bin:/usr/local/Cellar/smlnj/110.75/libexec/bin ]; then
-    export PATH="$PATH:/opt/X11/bin:/usr/local/Cellar/smlnj/110.75/libexec/bin"
-  fi
-
-  if [ -d $HOME/Library/Android/sdk ]; then
-    export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools:$HOME/Library/Android/sdk/tools"
-  fi
-fi
-
-export PATH="$HOME/bin:$PATH"
-
-export CODE="$HOME/code"
-
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export TERM=xterm-color
 export PAGER=less
+
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
+export PATH="$HOME/bin:$PATH"
+
+export CODE="$HOME/code"
 
 export EDITOR=vim
 export GIT_EDITOR="vim -u ~/.vimrc.tiny"
@@ -43,8 +32,6 @@ if [[ -n $BREW_PREFIX ]]; then
 fi
 
 if [[ $(uname) == "Darwin" ]]; then
-  export MARKDOWN_EDITOR="$(mdfind kMDItemCFBundleIdentifier=com.uranusjr.macdown | head -n1)/Contents/SharedSupport/bin/macdown"
-
   # the following function succeeds only when dark mode is enabled
   defaults read -g AppleInterfaceStyle >/dev/null 2>&1 && export DARK_MODE=1
 
