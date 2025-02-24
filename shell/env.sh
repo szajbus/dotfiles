df625a7122a613284ad90aa411ef3cb72f5d1b06
Michał Szajbe <michal.szajbe@gmail.com>
2025-02-24 21:26:49 +0100

    Use cursor as visual editor

--- shell/env.sh
+++ shell/env.sh
@@ -13,7 +13,7 @@ export GIT_EDITOR="vim -u ~/.vimrc.tiny"
 export MARKDOWN_EDITOR="$EDITOR"
 
 if [ "$SSH_CONNECTION" = "" ]; then
-  export VISUAL=code
+  export VISUAL=cursor
   export BUNDLER_EDITOR="$VISUAL"
 fi
 
